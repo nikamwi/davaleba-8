0cca41764734fc8d4e958c53fe7df119b0be69d8
nikamwi <nika.mwi2000@gmail.com>
2022-11-11 07:52:54 -0800

    gasagrdzelebeli

---
+++ js/mininav.js
@@ -0,0 +1,11 @@
+function showmininav(){
+    document.getElementById("navigacia").style.display = "block"
+    document.getElementById("x").style.display = "block"
+    document.getElementById("mininav").style.display = "none"
+}
+
+function closenav(){
+    document.getElementById("navigacia").style.display = "none"
+    document.getElementById("x").style.display = "none"
+    document.getElementById("mininav").style.display = "block"
+}
